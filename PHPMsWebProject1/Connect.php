e0b4c921aebe677e38a765590a767bd072b4d032
JAPISOFT <japisoft@hotmail.com>
2018-03-06 12:59:40 +0100

    last27

--- PHPMsWebProject1/Connect.php
+++ PHPMsWebProject1/Connect.php
@@ -7,11 +7,11 @@ $dbname = "test";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-//if ($conn->connect_error) {
-//    die("Connection failed: " . $conn->connect_error);
-//}
-//echo "Connected successfully<br>";
 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully<br>";
 
 ?>
