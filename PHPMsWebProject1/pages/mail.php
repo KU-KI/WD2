d998e77bcaf8fd1c8ce146457b14c9a5f238c1f0
JAPISOFT <japisoft@hotmail.com>
2018-04-18 09:35:29 +0200

    mail 2

--- PHPMsWebProject1/pages/mail.php
+++ PHPMsWebProject1/pages/mail.php
@@ -1,5 +1,5 @@
 <?php
-$to = "janpil44@gmail.com";
+$to = "japisoft@hotmail.com";
 $subject = "HTML email";
 
 $message = "
@@ -23,20 +23,36 @@ $message = "
 </html>
 ";
 
+
+//$name = $_POST['name'];
+//$email = $_POST['email'];
+//$message = $_POST['message'];
+//$from = 'From: yoursite.com';
+//$to = 'contact@yoursite.com';
+//$subject = 'Customer Inquiry';
+//$message = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+
 // Always set content-type when sending HTML email
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 // More headers
-$headers .= 'From: japisoft@hotmail.com' . "\r\n";
-//$headers .= 'Cc: myboss@example.com' . "\r\n";
-//$headers .= 'Bcc: myboss@example.com' . "\r\n";
-$headers .= 'Reply-To: japisoft@hotmail.com' . "\r\n";
-$headers .= 'X-Mailer: PHP/' . phpversion();
+//$headers .= 'From: japisoft@hotmail.com' . "\r\n";
+////$headers .= 'Cc: myboss@example.com' . "\r\n";
+////$headers .= 'Bcc: myboss@example.com' . "\r\n";
+//$headers .= 'Reply-To: japisoft@hotmail.com' . "\r\n";
+//$headers .= 'X-Mailer: PHP/' . phpversion();
 
 //mail($to,$subject,$message,$headers);
 //$emailSent = true;
 //if($emailSent == true)  echo "OK";
+
+$headers = array("From: from@example.com",
+    "Reply-To: replyto@example.com",
+    "X-Mailer: PHP/" . PHP_VERSION
+);
+$headers = implode("\r\n", $headers);
 
 if (mail ($to, $subject, $message, $headers)) {
 	echo '<p>Your message has been sent!</p>';
