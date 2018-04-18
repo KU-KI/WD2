e4b2ceda59d533f491c08a13917d068ac990b15e
JAPISOFT <japisoft@hotmail.com>
2018-04-18 10:11:55 +0200

    mail 4

--- PHPMsWebProject1/pages/mail.php
+++ PHPMsWebProject1/pages/mail.php
@@ -39,11 +39,11 @@ $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 $headers .= "X-Priority: 1\r\n";
 
 // More headers
-$headers .= 'From: japisoft@hotmail.com' . "\r\n";
-//$headers .= 'Cc: myboss@example.com' . "\r\n";
-//$headers .= 'Bcc: myboss@example.com' . "\r\n";
-$headers .= 'Reply-To: japisoft@hotmail.com' . "\r\n";
-$headers .= 'X-Mailer: PHP/' . phpversion();
+$headers .= "From: japisoft@hotmail.com" . "\r\n";
+//$headers .= "Cc: myboss@example.com" . "\r\n";
+//$headers .= "Bcc: myboss@example.com" . "\r\n";
+$headers .= "Reply-To: japisoft@hotmail.com" . "\r\n";
+$headers .= "X-Mailer: PHP/" . phpversion();
 
 //mail($to,$subject,$message,$headers);
 //$emailSent = true;
@@ -60,5 +60,43 @@ if (mail ($to, $subject, $message, $headers)) {
 } else {
 	echo '<p>Something went wrong, go back and try again!</p>';
 }
+
+
+//function send_mail($email, $recipient_name, $message='')
+//{
+//    require("phpmailer/class.phpmailer.php");
+
+//    $mail = new PHPMailer();
+
+//    $mail->CharSet="utf-8";
+//    $mail->IsSMTP();                                      // set mailer to use SMTP
+//    $mail->Host = "mail.example.com";  // specify main and backup server
+//    $mail->SMTPAuth = true;     // turn on SMTP authentication
+//    $mail->Username = "myusername";  // SMTP username
+//    $mail->Password = "p@ssw0rd"; // SMTP password
+
+//    $mail->From = "me@walalang.com";
+//    $mail->FromName = "System-Ad";
+//    $mail->AddAddress($email, $recipient_name);
+
+//    $mail->WordWrap = 50;                                 // set word wrap to 50 characters
+//    $mail->IsHTML(true);                                  // set email format to HTML (true) or plain text (false)
+
+//    $mail->Subject = "This is a Sampleenter code here Email";
+//    $mail->Body    = $message;
+//    $mail->AltBody = "This is the body in plain text for non-HTML mail clients";
+//    $mail->AddEmbeddedImage('images/logo.png', 'logo', 'logo.png');
+//    $mail->addAttachment('files/file.xlsx');
+
+//    if(!$mail->Send())
+//    {
+//        echo "Message could not be sent. <p>";
+//        echo "Mailer Error: " . $mail->ErrorInfo;
+//        exit;
+//    }
+
+//    echo "Message has been sent";
+//}
+
 
 ?>
