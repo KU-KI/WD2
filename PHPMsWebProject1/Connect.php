cb0875847fa40cfab7c16c3fccbfb3351fd011d7
JAPISOFT <japisoft@hotmail.com>
2019-03-21 13:27:16 +0100

    Azure MySQL in App added

--- PHPMsWebProject1/Connect.php
+++ PHPMsWebProject1/Connect.php
@@ -5,12 +5,23 @@ $username = "root";
 $password = "";
 $dbname = "test";
 
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+    
+    $servername = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $username = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $password = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Error No.".$conn->connect_errno." - Unable to connect to MySQL: ".$conn->connect_error);
 }
 //echo "Connected successfully<br>";
 
