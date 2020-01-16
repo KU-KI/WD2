8d70d6b87c683440d0d9c3ac63f8240b6b4d8064
Jan PILLAR <japisoft@hotmail.com>
2020-01-16 18:09:02 +0100

    160120

--- docker-APMP/www/index.php
+++ docker-APMP/www/index.php
@@ -3,13 +3,13 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>WD2-DOCKER</title>
+    <title>WD2-DOCKER APMP</title>
 </head>
 
 <body>
 
     <?php
-        echo "Ahoj WD2!";
+        echo "Ahoj WD2!<br><br>";
 
         $mysqli = new mysqli('db', getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), 'information_schema');
         if ($mysqli->connect_error) {
@@ -17,6 +17,16 @@
         } else {
             echo 'MySQLi Connected Successfully!';
         }
+
+        //$res = $conn->query("Select ITEM_NAME, ITEM_DESC, ITEM_ONHAND from MOE_ITEM_T");
+        //for ($row_no = 0; $row_no < $res->num_rows; $row_no++) {
+        //    $res->data_seek($row_no);
+        //    $row = $res->fetch_assoc();
+        //    echo " Item Name = " . $row['ITEM_NAME'] . " Item Description = " . $row['ITEM_DESC'] . " Item Onhand = " . $row['ITEM_ONHAND'];
+        //    echo "<br>";
+        //}
+        //$res->close();
+        //$conn->close();
     ?>
 
 </body>
---
+++ docker-APMP/www/phpinfo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="sk">
+
+<head>
+    <meta charset="utf-8" />
+    <title>WD2-DOCKER APMP</title>
+</head>
+
+<body>
+
+    <?php
+
+        phpinfo();
+
+    ?>
+
+</body>
+
+</html>
