66371f46026ed0e9b245626ed5b9b30cf91022ca
Jan PILLAR <japisoft@hotmail.com>
2020-02-26 10:31:24 +0100

    js2602

--- JS/javascript.php
+++ JS/javascript.php
@@ -6,21 +6,29 @@
     </head>
 
     <body>
-        ...
+        <div>
+            Prvý div ...
+        </div>
+        <div>
+            Druhý div ...
+        </div>
+        <a id="link">
+            Odkaz na presmerovanie ...
+        </a>
 
         <script>
-            //vráti pole všetkých elementov DOMu s hľadaným názvom
-            //divy[0] ... 
-            var divy = document.getElementsByTagName("div");
-            //vráti iba prvý element DOMu s hľadaným názvom
-            var div1 = document.getElementsByTagName("div")[0];
-            //vráti celkový počet nájdených elementov v DOMe
-            var pocet = document.getElementsByTagName("li").length;
-            //zmení obsah prvého elementu p
-            document.getElementsByTagName("p")[0].innerHTML = "Ahoj Webový dizajn!";
-            //vráti pole všetkých elementov v DOMe
-            //all[0] ...
-            var all = document.getElementsByTagName("*");
+            //nájde druhý element div
+            var wd2 = document.getElementsByTagName("div")[1];
+            var anchor = document.getElementById("link");
+            //vytvorí nový atribút elementu
+            var atribut = document.createAttribute("class");
+            var atribut1 = document.createAttribute("href");
+            //novému atribútu pridá hodnotu
+            atribut.value = "red";
+            atribut1.value = "http://www.ku.sk";
+            //nový atribút pridá k nájdenému elementu
+            wd2.setAttributeNode(atribut);
+            anchor.setAttributeNode(atribut1);
         </script>
     </body>
 
